7ad3b9bdfbd5b4c3a04150ef0d3e58fa276a406f
suraj <smileymaaa20@gmail.com>
2026-04-21 11:25:47 +0530

    removed flask, streamlit only

--- app.py
+++ app.py
@@ -1,40 +1,69 @@
-from flask import Flask, request, jsonify
+import streamlit as st
 import joblib
 import numpy as np
 
-app = Flask(__name__)
-
-# Load once
+# Load model
 model = joblib.load("models/lr.save")
 scaler = joblib.load("models/sc.save")
 
+st.title("Big Mart Sales Prediction")
 
+item_weight = st.number_input("Item Weight")
 
-@app.route("/")
-def home():
-    return "API Running..."
+item_fat_content = st.selectbox(
+    "Item Fat Content",
+    [("Low Fat",1),("Regular",2),("High Fat",0)],
+    format_func=lambda x: x[0]
+)[1]
 
-@app.route('/predict', methods=['POST'])
-def predict():
+item_visibility = st.number_input("Item Visibility")
 
-    data = request.json
+item_type = st.selectbox(
+    "Item Type",
+    [
+        ("Baking Goods",0),("Breads",1),("Breakfast",2),
+        ("Canned",3),("Dairy",4),("Frozen Foods",5),
+        ("Fruits and Vegetables",6),("Hard Drinks",7),
+        ("Health and Hygiene",8),("Household",9),
+        ("Meat",10),("Others",11),("Seafood",12),
+        ("Snack Foods",13),("Soft Drinks",14),("Starchy Foods",15)
+    ],
+    format_func=lambda x: x[0]
+)[1]
 
-    X = np.array([[
-        data['item_weight'],
-        data['item_fat_content'],
-        data['item_visibility'],
-        data['item_type'],
-        data['item_mrp'],
-        data['outlet_establishment_year'],
-        data['outlet_size'],
-        data['outlet_location_type'],
-        data['outlet_type']
-    ]])
+item_mrp = st.number_input("Item MRP")
+outlet_year = st.number_input("Outlet Establishment Year")
+
+outlet_size = st.selectbox(
+    "Outlet Size",
+    [("High",0),("Medium",1),("Small",2)],
+    format_func=lambda x: x[0]
+)[1]
+
+outlet_location_type = st.selectbox(
+    "Outlet Location Type",
+    [("Tier 1",0),("Tier 2",1),("Tier 3",2)],
+    format_func=lambda x: x[0]
+)[1]
+
+outlet_type = st.selectbox(
+    "Outlet Type",
+    [
+        ("Grocery Store",0),
+        ("Supermarket Type1",1),
+        ("Supermarket Type2",2),
+        ("Supermarket Type3",3)
+    ],
+    format_func=lambda x: x[0]
+)[1]
+
+if st.button("Predict"):
+
+    X = np.array([[item_weight,item_fat_content,item_visibility,
+                   item_type,item_mrp,outlet_year,
+                   outlet_size,outlet_location_type,outlet_type]])
 
     X_scaled = scaler.transform(X)
-    prediction = model.predict(X_scaled)
+    pred = model.predict(X_scaled)
 
-    return jsonify({'prediction': float(prediction[0])})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    st.success(f"Predicted Sales: {pred[0]:.2f}")
